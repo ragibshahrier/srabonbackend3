d697a86aa4fa32b0191f3b59a06cf5982d09ec57
Ragib Shahrier Bin Kabir <ragibshahrier2004123@gmail.com>
2025-05-23 09:31:21 +0600

    update build

--- build.sh
+++ build.sh
@@ -1,12 +1,42 @@
+# #!/usr/bin/env bash
+# # exit on error
+# set -o errexit
+
+# pip install -r requirements.txt
+
+# python manage.py collectstatic --no-input
+# python manage.py migrate
+# if [[ $CREATE_SUPERUSER ]];
+# then
+#   python manage.py createsuperuser --no-input --email "$DJANGO_SUPERUSER_EMAIL"
+# fi
+
+
+
 #!/usr/bin/env bash
 # exit on error
 set -o errexit
 
 pip install -r requirements.txt
 
+# Collect static files
 python manage.py collectstatic --no-input
-python manage.py migrate
-if [[ $CREATE_SUPERUSER ]];
-then
-  python manage.py createsuperuser --no-input --email "$DJANGO_SUPERUSER_EMAIL"
+
+# Run migrations
+python manage.py makemigrations --no-input
+python manage.py migrate --no-input
+
+# Create superuser only if required variables are set
+if [[ $CREATE_SUPERUSER == "true" ]]; then
+  echo "Creating superuser..."
+  python manage.py shell <<EOF
+from django.contrib.auth import get_user_model
+User = get_user_model()
+if not User.objects.filter(email="${DJANGO_SUPERUSER_EMAIL}").exists():
+    User.objects.create_superuser(
+        username="${DJANGO_SUPERUSER_USERNAME}",
+        email="${DJANGO_SUPERUSER_EMAIL}",
+        password="${DJANGO_SUPERUSER_PASSWORD}"
+    )
+EOF
 fi
